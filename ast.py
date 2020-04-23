e977dba6b467b68fb7e475b5b1589c59675676bd
jbpeek <jbpeek16@stlawu.edu>
2020-04-22 18:28:33 -0600

    Deletes clutter

--- ast.py
+++ ast.py
@@ -1,21 +1,6 @@
-"""
-SLU-C Abstract Syntax Trees
-
-An abstract syntax tree (AST) is a data structure that represents
-the concrete (text) syntax of a program
-
-Using a class hierarchy to represent language features in the grammar
-is called the "interpreter pattern"
-
-Singleton
-Strategy Pattern  (sorting strategy)
-Interpreter Pattern
-
-design patterns - catalog of best practices in software design
-
-"""
 from typing import Sequence, Union, Optional
 import operator
+
 
 # Use a class hierarchy to represent types.
 class Expr:
@@ -24,21 +9,9 @@ class Expr:
     """
     pass
 
-class Type:
-    pass
-
-class IntegerType(Type):
-    pass
-
-class FloatType(Type):
-    pass
-
-class BoolType(Type):
-    pass
-
 
 class FunctionDef:
-    def __init__(self, t, id:str, params, decls, stmts):
+    def __init__(self, t: str, id: IDExpr, params: Params, decls: DeclarationsExpr, stmts: StatementsStmt):
         # provide type hints for all of the parameters
         # Decls should be a dictionary
         # Key: id
@@ -57,8 +30,10 @@ class FunctionDef:
         for s in self.stmts:
             s.eval(env)  # TODO define environment
 
+
 class Stmt:
     pass
+
 
 class IfStmt(Stmt):
     def __init__(self, cond: Expr, truepart: Stmt, falsepart : Optional[Stmt]):
@@ -72,17 +47,21 @@ class IfStmt(Stmt):
             self.falsepart.eval(env)
 
 
+class StatementsStmt(Stmt):
+    def __init__(self):
+        pass
+
+
+
 class Declaration:
     pass
+
 
 class Program:
 
     def __init__(self, funcs: Sequence[FunctionDef]):
         self.funcs = funcs
 
-
-
-# TODO Don't just cut-and-paste new operations, abstract!
 
 class BinaryExpr(Expr):
     def __init__(self, left: Expr, right: Expr, op):
@@ -114,95 +93,6 @@ class BinaryExpr(Expr):
         }
         return opdict[self.op]
 
-class AndExpr(Expr):
-    def __init__(self, left: Expr, right: Expr):
-        self.left = left
-        self.right = right
-
-    def __str__(self):
-        return "({0} && {1})".format(str(self.left), str(self.right))
-
-
-    # def typeof(self) -> str: # where string is 'int' or 'float' or 'bool' or 'error'
-    # strings are not abstract "INT" "int" "fred"
-    # Expressions have types
-    #def typeof(self) -> Type:
-    def typeof(self) -> Union[int, bool, float]:   #
-        """
-        Return the type of the expression.
-        1) 4 + 4 is an int
-        2) 2 * 3.14 is a float
-        3) True && False is a bool
-        4) True && 3.14 type error
-        Static type checking: type check the program *before* we evaluate it.
-        Scheme is dynamically type checked. Type errors checked at run time.
-        Java - statically type checked.
-        C - static
-        Python - dynamic, checked at run time
-                 mypy - static type checker for Python
-        """
-        if self.left.typeof() == BoolType and self.right.typeof() == BoolType:
-            return BoolType
-        else:
-            # type error
-            raise SLUCTypeError(
-                "type error on line {0}, expected two booleans got a {1} and a {2}".format(0))
-
-class SLUCTypeError(Exception):
-    def __init__(self, message: str):
-        Exception.__init__(self)
-        self.message = message
-
-    def __str__(self):
-        return self.message
-
-class AddExpr(Expr):
-    def __init__(self, left: Expr, right: Expr):
-        self.left = left
-        self.right = right
-
-    def __str__(self):
-        return "({0} + {1})".format(str(self.left), str(self.right))
-
-    def scheme(self) -> str:
-        """
-        Return a string that represents the expression in Scheme syntax.
-        e.g.,  (a + b)   -> (+ a b)
-        """
-        return "(+ {0} {1})".format(self.left.scheme(), self.right.scheme())
-
-    def eval(self) -> Union[int,float]:
-        # TODO environment
-        return self.left.eval() +  self.right.eval()
-
-
-
-
-class MultExpr(Expr):
-    def __init__(self, left: Expr, right: Expr):
-        self.left = left
-        self.right = right
-
-    def __str__(self):
-        return "({0} * {1})".format(str(self.left), str(self.right))
-
-    def scheme(self):
-        """
-        Return a string that represents the expression in Scheme syntax.
-        e.g.,  (a * b)   -> (* a b)
-        """
-        return "(* {0} {1})".format(self.left.scheme(), self.right.scheme())
-
-    def eval(self) -> Union[int,float]:
-        # TODO environment
-        # Implementing SLU-C multiplication using Python's multiplication
-        # implmented * using mul instruction
-
-        # If we checked type when running eval we have a "dynamically typed"
-        # language
-
-        return self.left.eval() *  self.right.eval()
-
 
 class UnaryMinus(Expr):
     def __init__(self, tree: Expr):
@@ -216,6 +106,7 @@ class UnaryMinus(Expr):
 
     def eval(self):
         return -self.tree.eval()
+
 
 class IDExpr(Expr):
 
@@ -239,6 +130,7 @@ class IDExpr(Expr):
         # from the function definition (FunctionDef)
         pass
 
+
 class IntLitExpr(Expr):
 
     def __init__(self, intlit: str):
@@ -259,6 +151,14 @@ class IntLitExpr(Expr):
 
         #return IntegerType
         return int
+
+class SLUCTypeError(Exception):
+    def __init__(self, message: str):
+        Exception.__init__(self)
+        self.message = message
+
+    def __str__(self):
+        return self.message
 
 if __name__ == '__main__':
     """
